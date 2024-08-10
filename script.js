fa789bb757495bd0e715d5afe1a5f8df60569394
binayakbhandari <binayakbhandari60@gmail.com>
2024-08-10 19:21:27 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//This is my first day of 7 days JS Workshop.
+// To run the node js code enter "node filename.js"
+// To run the node js code automatically "node --watch filename.js"
+var a = 1
+var b = 3
+function add(a,b){
+    console.log("The sum is",a+b)
+}
+add(a,b)
