498967fa0f6bee92fa757a5820b61bda92a96a80
binayakbhandari <binayakbhandari60@gmail.com>
2024-08-12 23:14:27 +0545

    learn about regular function, arrow function, anonymous function, higher order function

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 // Thank You Digital Pathshala 
 
 
+
 // This is my 2nd day of 7 days JS Workshop.
 // Variable
 // Variable is created to store or hold the value.
@@ -75,6 +76,7 @@
 // Thank You Digital Pathshala 
 
 
+
 // This is my 3rd day of 7 days JS Workshop.
 // const numbers = [1,2,3,4,5,6,7,8,9]
 // slice() method
@@ -119,6 +121,7 @@
 // Thank You Digital Pathshala 
 
 
+
 // This is my 4th day of 7 days JS Workshop.
 // The code of line between opening and closing of curly braces id known as Block of Code.
 // Conditional Statements
@@ -137,13 +140,13 @@
 // isBusy && console.log("You are on the right path.") // To check true statement.
 // isBusy || console.log("You have been distracted.") // To check false statement.
 
-// Nullish coelescing operator
+// Nullish coalescing operator
 // const age = null
 // const result = age ?? "No age"
 // console.log("The age of the user is ",result)
 
 // Loops in JavaScript
-const names = ['Manish Sir', 'Binayak Sir', 'Saroj Sir', 'Prakash Sir']
+// const names = ['Manish Sir', 'Binayak Sir', 'Saroj Sir', 'Prakash Sir']
 // For Loop
 // for(var i=0; i < names.length; i++){
 //     console.log("I am ",names[i])
@@ -154,7 +157,263 @@ const names = ['Manish Sir', 'Binayak Sir', 'Saroj Sir', 'Prakash Sir']
 //     console.log("I am ",name)
 // }
 // This is more optimized than for loop.
-const shoes = ['Gold Star', 'Adidas', 'Nike', 'Puma']
-for(let shoe of shoes){
-    console.log("My favourite shoes brand is",shoe)
-}
+// const shoes = ['Gold Star', 'Adidas', 'Nike', 'Puma']
+// for(let shoe of shoes){
+//     console.log("My favourite shoes brand is",shoe)
+// }
+
+// Day 4 Code ends here.
+// Thank You Digital Pathshala 
+
+
+
+// This is my 5th day of 7 days JS Workshop.
+// Function is a block of code to perform some operation.
+// Types of function
+// Regular function or normal function
+// Parameter
+// function add(a,b){ // The data which is accepted by function is parameter i.e(a,b).
+//     console.log(a+b)
+// }
+// add(2,3) // The data which is given to function is argument i.e(2,3).
+
+// Named regular function
+// let subtract = function(a,b){
+//     console.log(a-b)
+// }
+// subtract(6,2)
+
+// Arrow function
+// const add = (a,b)=>{
+//     console.log(a+b)
+// }
+// add(4,6)
+
+// Anonymous function
+// Anonymous arrow function
+// (()=>{
+//     console.log("Hello World")
+// })()
+
+// Higher order function
+// Those functions where we have to give another function as an argument. or Those functions which take another function as a parameter.
+// - Higher order function ma gako function lai chai call back function vanenxa.
+// - Higher order function chai array ma lagxa.
+// forEach
+// map
+// reduce
+// filter
+
+// forEach function
+// const numbers = [1,2,3,4,5,6,7,8,9]
+// numbers.forEach(function(number){
+//     console.log(number)
+// })
+
+// const code = [
+//     {
+//         _id: "52435jsdk435245rdf452454",
+//         text: "test1",
+//         userId: "f3445134",
+//         title: "test1",
+//         ipAddress: "127.321.424.12",
+//         createdAt: "2024-08-12T14:09:29.236Z",
+//         updatedAt: "2024-08-12T14:09:29.236Z",
+//         __v: 0
+//     },
+//     {
+//         _id: "3498jsdk435245rdf452454",
+//         text: "test2",
+//         userId: "f345894",
+//         title: "test2",
+//         ipAddress: "127.321.424.12",
+//         createdAt: "2024-08-12T14:09:29.236Z",
+//         updatedAt: "2024-08-12T14:09:29.236Z",
+//         __v: 0
+//     },
+//     {
+//         _id: "3455jsdk435245rdf452454",
+//         text: "test3",
+//         userId: "f802455",
+//         title: "test3",
+//         ipAddress: "127.321.424.12",
+//         createdAt: "2024-08-12T14:09:29.236Z",
+//         updatedAt: "2024-08-12T14:09:29.236Z",
+//         __v: 0
+//     }
+// ]
+
+// code.forEach(function(data){
+//     console.log("The user's id is",data.userId)
+//     console.log("The IP address is", data.ipAddress)
+// })
+// With the help of forEach we can loop any array.
+
+// Map function
+// Quick Problem
+// There is an array and there is two objects and we have to add one key name and key value.
+// Before
+// const details = [
+//     {
+//         firstName: "Manish",
+//         lastName: "Basnet"
+//     },
+//     {
+//         firstName: "Reeya",
+//         lastName: "Basnet"
+//     }
+// ]
+// After
+// const details = [
+//     {
+//         firstName: "Manish",
+//         lastName: "Basnet",
+//         fullName: "Manish Basnet"
+//     },
+//     {
+//         firstName: "Reeya",
+//         lastName: "Basnet",
+//         fullName: "Reeya Basnet"
+//     }
+// ]
+
+// const details = [
+//     {
+//         firstName: "Manish",
+//         lastName: "Basnet"
+//     },
+//     {
+//         firstName: "Reeya",
+//         lastName: "Basnet"
+//     }
+// ]
+
+// const data = details.map(function(detail){
+//     return {
+//         // firstName: detail.firstName,
+//         // lastName: detail.lastName,
+//         ...detail,
+//         // spread operator is used to show all the keys and keys value as it is of the parameter.
+//         fullName: detail.firstName + " " + detail.lastName
+//     }
+// })
+// console.log(data)
+// // Note:- Map function does not modifies the original array.
+// // It creates a new array and stores data in it.
+// // If we want to modify and create new data then map is the best.
+
+// Quick Problem
+// There is an array and there are theree objects and we have to add the new key and key value by multiplying price and quantity.
+// Before
+// const products = [
+//     {
+//         name : "Apple",
+//         price : 200,
+//         quantity : 2
+//     },
+//     {
+//         name : "Mango",
+//         price : 100,
+//         quantity : 5
+//     },
+//     {
+//         name : "Pineapple",
+//         price : 300,
+//         quantity : 1
+//     }
+// ]
+// After
+// const products = [
+//     {
+//         name : "Apple",
+//         price : 200,
+//         quantity : 2,
+//         total : 400
+//     },
+//     {
+//         name : "Mango",
+//         price : 100,
+//         quantity : 5,
+//         total : 400
+//     },
+//     {
+//         name : "Pineapple",
+//         price : 300,
+//         quantity : 1,
+//         total : 300
+//     }
+// ]
+
+// const products = [
+//     {
+//         name : "Apple",
+//         price : 200,
+//         quantity : 2
+//     },
+//     {
+//         name : "Mango",
+//         price : 100,
+//         quantity : 5
+//     },
+//     {
+//         name : "Pineapple",
+//         price : 300,
+//         quantity : 1
+//     }
+// ]
+
+// const fruits = products.map(function(fruit){
+//     return {
+//         ...fruit,
+//         total : fruit.price * fruit.quantity
+//     }
+// })
+// console.log(fruits)
+
+// Quick Problem
+// Before
+// const nums = [1,2,3,4,5]
+// After
+// const squares = [1,2,3,4,5]
+
+// const nums = [1,2,3,4,5]
+// const squares = nums.map(function(num){
+//     return num * num
+// })
+// console.log(nums)
+// console.log(squares)
+
+// Filter function
+// It helps to filter data from the array and return data in a new array.
+// const numbers = [1,2,3,4,5,6,7,8,9]
+// const even = numbers.filter(function(num){
+//     return num % 2 == 0
+// })
+// console.log(even)
+
+// const books = [
+//     {
+//         title : 'Mahabir Pun',
+//         year : 2023
+//     },
+//     {
+//         title : 'Rich Dad Poor Dad',
+//         year : 1997
+//     },
+//     {
+//         title : 'The Alchemist',
+//         year : 1988
+//     },
+//     {
+//         title : 'Muna Madan',
+//         year : 1936
+//     }
+// ]
+
+// const publishedBook = books.filter(function(book){
+//     return book.year > 2000
+// })
+// console.log(publishedBook)
+
+// Day 5 Code ends here.
+// Thank You Digital Pathshala 
